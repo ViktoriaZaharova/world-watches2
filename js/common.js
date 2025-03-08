97034b38e43b5680762002899c30ef8260e91db7
Viktoria Zaharova <zaharova181114@gmail.com>
2025-03-09 00:01:10 +0200

    + sections

--- js/common.js
+++ js/common.js
@@ -1,2 +1,16 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+$('.brands-slider').slick({
+  slidesToShow: 4,
+  appendArrows: '.brands-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"></button>',
+  nextArrow: '<button type="button" class="slick-next"></button>'
+});
+
+
+$('.product-slider').slick({
+  slidesToShow: 6,
+  appendArrows: '.product-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"></button>',
+  nextArrow: '<button type="button" class="slick-next"></button>'
+});
